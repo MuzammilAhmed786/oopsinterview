2065cc87355d99bbad5c1426eb233f50c54a4244
MuzammilAhmed786 <ahmed.muzammil@gmail.com>
2021-04-12 06:57:08 +0000

    first time

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace jfk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            first f = new first();
+            Console.WriteLine(first.hellothere(2));
+            first.printone();
+
+            first ff = new first();
+            first.printone();
+        }
+    }
+
+    class first 
+    {
+        public static int one = 1;
+        public static string hellothere(int p)
+        {
+            one = p;
+            return "Hello there" + p;
+        }
+
+        public static void printone()
+        {
+            Console.WriteLine(one);
+        }
+
+
+    }
+
+
+    public class second
+    {
+       const int i = 0;
+       readonly int b;
+
+        int operation()
+        {
+           return 0;
+        }
+
+        
+
+    }
+    
+}
